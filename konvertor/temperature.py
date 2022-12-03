ca67c5f6e0d862459e9862d84e2500ed1050eb1b
amirali-ashraf <amiraliash.dev@gmail.com>
2022-12-03 13:55:12 -0500

    fix type and add gitignore

--- konvertor/temperature.py
+++ konvertor/temperature.py
@@ -1,6 +1,6 @@
 
 def fahrenheit_to_clesius(fahrenheit):
-    raise NotImplementedMethod("Method needs to be implemented")
+    raise NotImplementedError("Method needs to be implemented")
 
 def clesius_to_fahrenheit(clesius):
-    raise NotImplementedMethod("Method needs to be implemented")
+    raise NotImplementedError("Method needs to be implemented")
