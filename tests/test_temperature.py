10891b7cc861420137729f60338bd1f0cf65dd38
amirali-ashraf <amiraliash.dev@gmail.com>
2022-12-03 14:06:46 -0500

    fix import and test

--- tests/test_temperature.py
+++ tests/test_temperature.py
@@ -1,12 +1,12 @@
 import unittest
-from konvertor import *
+from konvertor.temperature import *
 
 class TestTemperature(unittest.TestCase):
-    def test_fahrenheit_to_celesius(fahrenheit):
-        self.assertEqual(fahrenheit_to_celesius(32), 0, 'should be 0')
+    def test_fahrenheit_to_celesius(self):
+        self.assertEqual(fahrenheit_to_clesius(32), 0, 'should be 0')
 
-    def test_celesius_to_fahrentheit(celesius):
-        self.assertEqual(celesius_to_fahrenheit(0), 32, 'should be 0')
+    def test_celesius_to_fahrentheit(self):
+        self.assertEqual(clesius_to_fahrenheit(0), 32, 'should be 32')
 
 
 
